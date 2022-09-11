9ae37f7a14da0109ec8719e4558a4eb5e299ddac
Nirwana Sulaiman <annafeu93@gmail.com>
2022-09-11 11:44:23 +0800

    localhost2000

--- backend/main.js
+++ backend/main.js
@@ -5,12 +5,12 @@ let app = express();
 app.use(express.json());
 app.use(router);
                             // or can simply const api = require("./api"); --> app.use(api.router);
-app.listen(3000, (error) => {
+app.listen(2000, (error) => {
     if (error) {
         console.log(error);
         process.exit(0);
     }
     else {
-        console.log("Server started on port: 3000");
+        console.log("Server started on port: 2000");
     }
 });
